1d60122424ed5df20b5b9a40059f6c57150f86de
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2024-07-10 01:24:55 +0600

    done

---
+++ c++/oop/class.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Student{
+    public: 
+    string name;
+    int roll;
+};
+
+int main(){
+    Student rafe; 
+    rafe.name = "Rafe Uddaraj";
+    rafe.roll = 672641;
+    cout << rafe.name <<endl;
+    cout << rafe.roll <<endl;
+    return 0;
+}
