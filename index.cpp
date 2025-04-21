65a5f31e6382c448cc2b7970e38968b389bebaaf
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2025-04-21 13:37:29 +0600

    feat: done

---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int a = 10;
+    int b = 20; 
+    int result = a + b; 
+    cout << "The sum of " << a << " and " << b << " is: " << result << endl;
+    return 0;
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+// Ex 1:
+let a = 10;
+let b = 20;
+let result = a + b;
+console.log(result); // Output: 30
+
+// Operation Calculation
+// let a = 10 ;  1
+// let b = 20 ; 1
+// let result = a + b ; 2
+
+// total calculation = 1 + 1 + 2 = 4
+// time complexity = O(4) = O(1)
+
+// ---------------------------------
+
+// Ex 2:
+let n = 10;
+for (let i = 0; i < n; i++) {
+  console.log(i);
+}
+
+//  Operation Calculation
+// let n = 10 ; 1
+// for (let i = 0; i < n; i++) { ; 1
+//   console.log(i); ; 1
+// }
+
+// Big O Calculation
+// total calculation = Big O (1)
+
